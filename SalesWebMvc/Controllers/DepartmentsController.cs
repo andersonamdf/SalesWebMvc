730da3635f03e8b7468b7c23ec46c95b74127716
Anderson Miranda <anderson.miranda@vsoft.com.br>
2026-05-03 10:57:13 -0500

    aula 3

---
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            List<Department> list = new List<Department>();
+            list.Add(new Department { Id = 1, Nome = "Eletronics" });
+            list.Add(new Department { Id = 2, Nome = "Fashion" });
+            return View(list);
+        }
+    }
+}
